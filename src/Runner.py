35302ec7151a081d08c70b99c64687553cc063ae
Jack Coy <jackman3000@gmail.com>
2018-08-19 00:15:50 +1000

    Remove tasks from bash history

--- src/Runner.py
+++ src/Runner.py
@@ -63,7 +63,6 @@ def scroll_down():
 os.system('tmux resize-pane -t ' + tmuxSessionName + ':.1 -y 10')
 
 send_keys("PS1='$(echo -e \"\e[1m\e[32m\$(date +%H:%M:%S)\e[0m | \e[94mTASK BASHER\e[0m | Executing... \")' Enter")
-send_keys("unset HISTFILE' Enter")
 execute('clear')
 os.system('tmux clear-history -t ' + tmuxSessionName + ':.0')
 
